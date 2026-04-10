38a6b731219aa6de7206c86b056ecb05140193cd
Ayodeji Isijola <ayodejiisijola@gmail.com>
2026-04-10 15:23:01 +0100

    01 setup

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+
+export default function Home() {
+  return (
+    <div>
+      <p className="text-purple-500">
+        HELLO WORLD!
+      </p>
+   </div>
+  );
+}
